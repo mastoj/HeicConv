4811effe7fe143e9e7f906c19b5449937aee5afa
Tomas Jansson <tomas.jansson@unacast.com>
2019-08-01 23:31:01 +0200

    Bump version to 0.2.1

--- src/HeiConv/AssemblyInfo.fs
+++ src/HeiConv/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("HeiConv")>]
 [<assembly: AssemblyProductAttribute("HeiConv")>]
 [<assembly: AssemblyDescriptionAttribute("HEIC to JPG file converter. The goal for this application is simplicity over a lot of functionality.")>]
-[<assembly: AssemblyVersionAttribute("0.2.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
+[<assembly: AssemblyVersionAttribute("0.2.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "HeiConv"
     let [<Literal>] AssemblyProduct = "HeiConv"
     let [<Literal>] AssemblyDescription = "HEIC to JPG file converter. The goal for this application is simplicity over a lot of functionality."
-    let [<Literal>] AssemblyVersion = "0.2.0"
-    let [<Literal>] AssemblyFileVersion = "0.2.0"
+    let [<Literal>] AssemblyVersion = "0.2.1"
+    let [<Literal>] AssemblyFileVersion = "0.2.1"
